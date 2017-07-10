cc8009bb8aedd4a5944e78339674d898640ef4f3
Bruno Moscão <bruno@b-guest.com>
2017-07-10 19:16:10 +0100

    Added some tests

--- XUnitTestProject1/UnitTest1.cs
+++ XUnitTestProject1/UnitTest1.cs
@@ -8,6 +8,13 @@ namespace XUnitTestProject1
         [Fact]
         public void Test1()
         {
+            Assert.InRange(5, 1, 10);
+        }
+
+        [Fact]
+        public void FailingTest1()
+        {
+            Assert.True(false);
         }
     }
 }
