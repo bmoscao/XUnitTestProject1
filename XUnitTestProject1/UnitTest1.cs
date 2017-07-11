09632c899f115b364094b1bfc2f9a6bc1af2a116
bmoscao <bmoscao@gmail.com>
2017-07-11 18:29:31 +0100

    * Settings.cs: Added tests calling the API
    * UnitTest1.cs:
    * XUnitTestProject1.csproj:

--- XUnitTestProject1/UnitTest1.cs
+++ XUnitTestProject1/UnitTest1.cs
@@ -5,10 +5,17 @@ namespace XUnitTestProject1
 {
     public class UnitTest1
     {
+        
+
         [Fact]
         public void Test1()
         {
-            Assert.InRange(5, 1, 10);
+            var client = ClientFactory.CreateApiClient();
+            client.LoginWithUser("John", "12345");
+            client.ReadUserTypologyById("8C618DD3-84B1-4116-A6ED-0DF9998422BD")
+                  .AssertThatExists()
+                  .AssertFieldIsEqualTo("Name", "Account")
+                  .AssertFieldIsEqualToCaseInsensitive("Id", "8C618DD3-84B1-4116-A6ED-0DF9998422BD");
         }
 
         [Fact]
